76c7d805e1aa4c197e14295de8083b25321a64eb
Venkateswarlu Dhanala <d4dhanala@gmail.com>
2022-02-04 18:05:19 -0600

    modified for both TestNG framework & Junit Cucumber framework

--- src/test/java/selenium/web/automation/RunCucumberTest.java
+++ src/test/java/selenium/web/automation/RunCucumberTest.java
@@ -1,20 +1,19 @@
-package student.marks.evaluation;
+package selenium.web.automation;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 
 /*******************************************************************************
  * Copyright (c) 2022 This project is belongs to 'Venkateswarlu Dhanala'
- * Created on : 02/01/2022
- * Last modified on : 02/01/2022
+ * Created on : 2/3/22
+ * Last modified on : 2/4/22
  * Owned by : Venkateswarlu Dhanala
  ******************************************************************************/
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/", glue = {"student.marks.evaluation.steps"}, tags =
-    "@MarksEvaluation", plugin = {"pretty","html:target/TestResults/cucumber.html",
+@CucumberOptions(features = "src/test/resources/", glue = {"selenium.web.automation.steps"}, tags =
+    "@AmazonProductSearch", plugin = {"pretty", "html:target/TestResults/cucumber.html",
     "json:target/TestResults/cucumber.json"})
 public class RunCucumberTest {
 }
